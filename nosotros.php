0c8f5dffcc01212727f62d52ebd559e3bed89bb1
Nehuensito <nehuenchibiurra@gmail.com>
2025-06-04 16:40:27 -0300

    .

--- nosotros.php
+++ nosotros.php
@@ -7,6 +7,19 @@
     <title>Sobre nosotros</title>
 </head>
 <body>
-    
+    <section class="nosotros">
+  <div class="contenedor-nosotros">
+    <h2>Quiénes Somos</h2>
+    <p>
+      Somos una empresa comprometida con brindar soluciones innovadoras y eficientes. Nuestro equipo está conformado por profesionales apasionados por la tecnología, el diseño y el desarrollo web.
+    </p>
+    <p>
+      Desde nuestros inicios, nos hemos enfocado en crear experiencias digitales únicas que conecten a las personas con las marcas de forma significativa.
+    </p>
+    <p>
+      Creemos en la transparencia, la creatividad y la mejora constante. Nuestro objetivo es crecer junto a nuestros clientes y ayudarlos a alcanzar sus metas.
+    </p>
+  </div>
+</section>
 </body>
 </html>
